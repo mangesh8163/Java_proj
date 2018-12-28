f3129a7b0a580d480cf64b155d270d9beb871e37
mangesh8163 <46221318+mangesh8163@users.noreply.github.com>
2018-12-28 18:06:37 +0530

    added controller method

--- Controllerr.java
+++ Controllerr.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Controllerr {
+public class Controller {
 	
 	
 	@RequestMapping("/home.htm")
